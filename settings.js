c52bd7b8491023fd2c770c25e3b868d79afda486
idu00002 <104816492+idu00002@users.noreply.github.com>
2022-07-18 18:59:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,10 +36,10 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['94765251168'] //ur owner number
+global.ownername = "🦄*•.¸♡ ۝⃟̥̩̩̩̥̩̥͚🌹⃝❤️࿔⃟࿔𝓘𝓭𝓾𝓻𝓪𝓷𝓰𝓪 ⃟⌘࿔⃝❤️   ♡¸.•*" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['94765251168']
+global.ownertag = '94765251168' //ur tag number
+global.botname = 'mr.baby🤓' //ur bot name
+global.linkz = "http://wa.me/+94777511834?text=°◕𝓗𝓘-යකූ-නම-ගම-කියන්න-සෙවු-කරගන්න" //your theme url which will be displayed on whatsapp
+global.websitex = "http://wa.me/+94777511834?text=°◕𝓗𝓘-යකූ-නම-ගම-කියන්න-සෙවු-කරගන්න" //ur website to be displayed
+global.botscript = 'http://wa.me/+94777511834?text=°◕𝓗𝓘-යකූ-නම-ගම-කියන්න-සෙවු-කරගන්න' //script link
+global.reactmoji = "☹️" //ur menu react emoji
+global.themeemoji = "😌" //ur theme emoji
+global.packname = " idu baba🤓 " //ur sticker watermark packname
+global.author = "🦄idu baba🤓" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['94765251168'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
